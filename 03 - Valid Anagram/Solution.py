e95f674250caf5c53225de0162a28d4ab2018d87
Gayathri-katneni <gayathrikatneni@gmail.com>
2026-08-05 18:04:56 +0530

    Day 3: Valid Anagram

---
+++ 03 - Valid Anagram/Solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        countS = {}
+        countT = {}
+
+        for ch in s:
+            countS[ch] = countS.get(ch, 0) + 1
+
+        for ch in t:
+            countT[ch] = countT.get(ch, 0) + 1
+
+        return countS == countT

c21ec0ef0c6a34034df40e245327b2f83e66c7b4
Gayathri-katneni <gayathrikatneni@gmail.com>
2026-08-08 13:58:24 +0530

    Day 5: Majority element

---
+++ 05 - Majority Element/Solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = {}
+        for num in nums:
+            count[num] = count.get(num,0)+1
+        for num in count:
+            if count[num] > len(nums) / 2:
+                return num
